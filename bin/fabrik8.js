5cae8d8873814e7e0d68ff0acb403ac3e8b00feb
Alexander Early <alex@npmjs.com>
2019-07-12 17:31:54 -0700

    use bistre

--- bin/fabrik8.js
+++ bin/fabrik8.js
@@ -1,28 +1,13 @@
 #!/usr/bin/env node
 
 const fabricator = require('../lib/index')
-const chalk = require('chalk')
+const bistre = require('bistre')()
 require('dotenv').config()
-
-const levelColors = {
-  debug: 'gray',
-  info: 'white',
-  warn: 'yellow',
-  error: 'red'
-}
-
-const debugOut = {
-  write: function (data) {
-    const entry = JSON.parse(data)
-    const levelColor = levelColors[entry.level]
-    console.log(`${chalk[levelColor](entry.time)} - ${chalk[levelColor](entry.level)} ${entry.message}`)
-  }
-}
 
 require('yargs') // eslint-disable-line no-unused-expressions
   .usage('$0 <command> [options]')
-  .command(require('../lib/commands/init')(fabricator, debugOut))
-  .command(require('../lib/commands/create')(fabricator, debugOut))
+  .command(require('../lib/commands/init')(fabricator, bistre))
+  .command(require('../lib/commands/create')(fabricator, bistre))
   .demandCommand(1, 'A command is required.')
   .help()
   .version()
