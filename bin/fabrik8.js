bc72c2ba533c14c97b3e1d294f342e49ad3b4ed3
Michael Garvin <gar+gh@danger.computer>
2019-07-24 15:54:24 -0700

    chore(refactor): move commands into bin
    remove one layer of DI

--- bin/fabrik8.js
+++ bin/fabrik8.js
@@ -1,14 +1,10 @@
 #!/usr/bin/env node
 
-const fabricator = require('../lib/index')
-const bistre = require('bistre')()
-bistre.pipe(process.stdout)
-
 require('dotenv').config()
 
 require('yargs') // eslint-disable-line no-unused-expressions
   .usage('$0 <command> [options]')
-  .command(require('../lib/commands/create')(fabricator, bistre))
+  .command(require('./cmds/create'))
   .demandCommand(1, 'A command is required.')
   .help()
   .version()
