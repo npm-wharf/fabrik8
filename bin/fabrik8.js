df86fd08e8447845f7bd37e37021123e7e1158b8
Gar <gar+gh@danger.computer>
2019-07-25 07:54:41 -0700

    chore(refactor): move commands into bin (#17)
    * chore(refactor): move commands into bin
    
    remove one layer of DI
    
    * chore(refactor): use commandDir
    
    * chore(refactor): move common args to base command
    
    * chore(refactor): no more globals

--- bin/fabrik8.js
+++ bin/fabrik8.js
@@ -1,15 +1,33 @@
 #!/usr/bin/env node
 
-const fabricator = require('../lib/index')
-const bistre = require('bistre')()
-bistre.pipe(process.stdout)
-
 require('dotenv').config()
 
 require('yargs') // eslint-disable-line no-unused-expressions
   .usage('$0 <command> [options]')
-  .command(require('../lib/commands/create')(fabricator, bistre))
+  .commandDir('cmds')
   .demandCommand(1, 'A command is required.')
   .help()
   .version()
+  .options({
+    vaultHost: {
+      description: 'the host of the vault server containing sensitive cluster information, auth data, and defaults',
+      default: process.env.VAULT_HOST
+    },
+    vaultToken: {
+      description: 'an auth token for the vault server',
+      default: process.env.VAULT_TOKEN
+    },
+    vaultRoleId: {
+      description: 'an AppRole id token for the vault server',
+      default: process.env.VAULT_ROLE_ID
+    },
+    vaultSecretId: {
+      description: 'an AppRole secret id token for the vault server',
+      default: process.env.VAULT_SECRET_ID
+    },
+    vaultSecretPrefix: {
+      description: 'the prefix for the KV secrets engine',
+      default: process.env.VAULT_SECRET_PREFIX || 'kv/'
+    }
+  })
   .argv
